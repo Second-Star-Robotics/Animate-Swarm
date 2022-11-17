da54395e973369f20605cce5f4a0dd747c2e3928
eberkenpas <eric@secondstarrobotics.com>
2022-11-17 15:06:53 -0500

    Cleaned up directory

---
+++ Outputs/Readme.txt
@@ -0,0 +1,1 @@
+This folder is where all output animations are saved.
--- animate_swarm_Demo.m
+++ animate_swarm_Demo.m
@@ -1,59 +1,28 @@
 %Execute Cong's script and extract variables
 Density_estimation_lastlast
 
-Input_Data.T_space = T_space;
-Input_Data.x_esti = x_esti;
-Input_Data.Zeta = Zeta;
+%Assemble Input Data Structure
+Input_Data.Density_Plot.T_space = T_space;
+Input_Data.Density_Plot.x_esti = x_esti;
+Input_Data.Density_Plot.Zeta = Zeta;
 Input_Data.t_space = t_space;
-Input_Data.platform(1).z = z(1,:);
-Input_Data.platform(2).z = p_e_max;
-Input_Data.layer.z = p_max;
+Input_Data.Platform(1).z = z(1,:);
+Input_Data.Platform(2).z = p_e_max;
+Input_Data.Layer.z = p_max;
+Input_Data.error = error; %Consider changing the name of this variable as it's a matlab command
 
-%MOVE THIS DOWN AND EXTRACT INPUT VARIABLES FIRST
-
-figure(1)
-plot(t_space,tr_C,'LineWidth',2);
-xlabel('Time (hr)','FontSize',20)
-ylabel('trace(C)','FontSize', 40,'Interpreter','latex')
-title('Measurement updated every 10 min','FontSize',20)
-figure(2)
-% plot(t_space,mean(Rho_error,1),'LineWidth',2);
-plot(t_space(1:k-1),error(1:k-1),'LineWidth',2);
-xlabel('Time (hr)','FontSize',20)
-ylabel('Error','FontSize', 40,'Interpreter','latex')
-title('Measurement updated every 10 min','FontSize',20)
-keyboard;
-%
-
+%Clean up workspace
 close all;
 clearvars -except Input_Data
 
-%Extract Data
-T_space = Input_Data.T_space;
-x_esti = Input_Data.x_esti;
-Zeta = Input_Data.Zeta;
-t_space = Input_Data.t_space;
-z1 = Input_Data.platform(1).z;
-z2 = Input_Data.platform(2).z;
-z_layer = Input_Data.layer.z;
+%Set input parameters
+platform_stl = '190927 Driftcam.stl';
+layer_stl = 'squid.stl';
+video_filename = 'test.avi';
+speed = 2;
+platform_scale = 60;
+layer_scale = 75;
+smoothing = 15;
 
-index = 1000;
+animate_result(Input_Data, platform_stl, layer_stl, video_filename, speed, platform_scale, layer_scale, smoothing);
 
-%Plot Density
-hold on
-h = surf(T_space,Zeta,(x_esti));
-view(0,90)
-shading interp
-colorbar
-co = colorbar;
-co.Label.String = 'Density';
-%co.FontSize = 20;
-
-yticks([-800 -700 -600 -500 -400 -300 -200 -100 0]);
-yticklabels({'800' '700' '600' '500' '400' '300' '200' '100' '0'})
-ylabel('Depth [m]');
-xlabel('Time [hrs]');
-
-p2 = plot3(t_space(1:index),z1(1:index),250*ones(1,length(t_space(1:index))),'LineStyle','--','Color','r');
-p1 = plot3(t_space(1:index),z_layer(1:index),250*ones(1,length(t_space(1:index))),'k','LineWidth',2,LineStyle='--');
-p3 = plot3(t_space(1:index),z2(1:index),250*ones(1,length(t_space(1:index))),'b','LineWidth',2);
